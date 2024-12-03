776e00482ccb3d3dc8b83f2710882068d5ae11de
Mariia <1mpellee@gmail.com>
2024-12-03 21:10:21 +0200

    first commit

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+      const type = types[i];
+      const typeName = type.name.toLowerCase();
+        if (typeof arg !== typeName) {
+            throw new Error(`Wrong argument type! Expected ${typeName}`);
+        }
+    }
+    const res = fn(...args);
+    const lastType = types[types.length - 1];
+    const lastTypeName = lastType.name.toLowerCase();
+    if (typeof res !== lastTypeName) {
+        throw new Error(`Wrong result type! Expected ${lastTypeName}`);
+    }
+    return res;
+};
 
 module.exports = { contract };
